a0e8999beebd040adf6eaf6fabee064641597c12
Avirup Dasgupta <avirupdasgupta3628@gmail.com>
2025-10-15 21:43:59 +0530

    Final cleanup: Remove old files, add monitoring results, exclude DVC storage

--- debt_collection_ml/dagshub_config.py
+++
@@ -1,9 +0,0 @@
-
-# Auto-generated DagsHub configuration
-REPO_OWNER = "avirup112"
-REPO_NAME = "Demo-Banking"
-REPO_URL = "https://dagshub.com/avirup112/Demo-Banking"
-
-# Initialize DagsHub tracker
-from src.utils.dagshub_integration import DagsHubTracker
-tracker = DagsHubTracker(REPO_OWNER, REPO_NAME)
